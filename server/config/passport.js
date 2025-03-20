eb4eceb6d7598536759748a8043b12e59100169e
dhaxanxxt <mohn08052006@gmail.com>
2025-03-20 15:38:45 +0530

    All Files Added

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,7 +5,7 @@
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: 'https://kepom.onrender.com/auth/google/callback',
         scope: ["profile", "email"]
     },
     function (accessToken, refreshToken, profile, callback){
