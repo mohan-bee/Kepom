1edebd16c35ca8d65a84f10e410ae0c9da38e4d2
dhaxanxxt <mohn08052006@gmail.com>
2025-03-21 11:39:04 +0530

    Removed Authentication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Group, Home, Plus, Search, User } from 'lucide-react'
+import { Group, Home, Music, Plus, Search, User } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -8,6 +8,7 @@ const Navbar = () => {
     <div className="md:hidden flex justify-around w-full mb-5 bg-gray-800 py-4 rounded-xl">
                <p className="bg-gray-700 text-white p-3 rounded-full border cursor-pointer" onClick={() =>  navigate('/')}><Home /></p>
                <p className="bg-gray-700 text-white p-3 rounded-full border cursor-pointer" onClick={() => navigate('/add-playlist')}><Plus /></p>
+               <p className="bg-gray-700 text-white p-3 rounded-full border cursor-pointer" onClick={() => navigate('/add-music')}><Music /></p>
                <p className="bg-gray-700 text-white p-3 rounded-full border cursor-pointer" onClick={() => navigate('/rooms')}><Group /></p>
                <p className="bg-gray-700 text-white p-3 rounded-full border cursor-pointer" onClick={() => navigate('/profile')}><User /></p>
              </div>
